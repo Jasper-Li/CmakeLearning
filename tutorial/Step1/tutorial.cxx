00e22591ab06afc4e26a9969e94bd7dbefeb3042
Xinghe Li <lixinghe66@gmail.com>
2021-12-16 22:20:36 +0800

    finish step1

--- tutorial/Step1/tutorial.cxx
+++ tutorial/Step1/tutorial.cxx
@@ -3,11 +3,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include "TutorialConfig.h"
 
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    std::cout << "Version: " << VERSION_MAJOR << "." << VERSION_MINOR << std::endl;
+    std::cout << "CXX VERSION: " << __cplusplus << std::endl;
     return 1;
   }
 
